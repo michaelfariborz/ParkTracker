e9d408c574a1ac52fe63c9adf4af1e0e031ff908
Michael Fariborz <michael.fariborz@gmail.com>
2026-04-19 21:04:49 -0500

    fix: add input validation to setTheme

--- ParkTracker/wwwroot/js/theme.js
+++ ParkTracker/wwwroot/js/theme.js
@@ -1,6 +1,7 @@
 window.getTheme = () => document.documentElement.getAttribute('data-bs-theme') ?? 'light';
 
 window.setTheme = (theme) => {
+    if (theme !== 'light' && theme !== 'dark') return;
     document.documentElement.setAttribute('data-bs-theme', theme);
     localStorage.setItem('theme', theme);
 };
